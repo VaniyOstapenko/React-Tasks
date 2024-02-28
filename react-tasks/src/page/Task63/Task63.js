8699877bbb103f1d1e78e2483befc1d0ad0f1aba
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-02-28 15:16:49 +0300

    React-Tasks

--- react-tasks/src/page/Task63/Task63.js
+++ react-tasks/src/page/Task63/Task63.js
@@ -3,12 +3,26 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Task63() {
-   
+    const [time, setTime] = useState('');
+
+    function getTime() {
+        const date = new Date();
+        const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+        const minuts = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+        setTime(`${hours}:${minuts}:${seconds}`);
+    }
+
+    useEffect(() => {
+        setInterval(() => getTime(), 1000);
+    }, [])
     return (
         <>
             <h1>Задача 63</h1>
-            <p></p>
+            <p>Таймер с использованием useState и useEffect: Создайте компонент, который отображает текущее время и обновляет его
+                каждую секунду.</p>
 
+            <h1>{time}</h1>
             <p><Link to='/'>HomePage</Link></p>
         </>
     );
