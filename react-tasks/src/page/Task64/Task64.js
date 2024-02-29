d5c590b526b00887688aed48b2763c9bfcd77561
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-02-29 12:57:46 +0300

    React-Tasks

--- react-tasks/src/page/Task64/Task64.js
+++ react-tasks/src/page/Task64/Task64.js
@@ -3,12 +3,25 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Task64() {
+    const [num, setNum] = useState(null);
+
+    async function axiosRequest() {
+        const randomNum = Math.floor(Math.random() * 100);
+        const response = await axios.get(`http://numbersapi.com/${randomNum}`)
+        setNum(response.random)
+    }
+
+    useEffect(() => {
+        console.log(axiosRequest(num))
+    })
+
 
     return (
         <>
             <h1>Задача 64</h1>
-            <p>ФормавводасиспользованиемuseStateиuseEffect:СоздайтекомпонентReact, которыйсодержитформусполемввода.ИспользуйтехукuseStateдляхранения значениявведенноготекстаихукuseEffectдляотслеживанияизмененийэтого значения.Прикаждомизменениизначениявполеввода,выводитьеговконсоль спомощьюuseEffect.</p>
-
+            <p>Факты с использованием useState и useEffect: http://numbersapi.com/:id. Создайте компонент React,
+                который при первичном рендеринге отправляет запрос к API с рандомно сгенерированным числом и отображает
+                результат в консоль.</p>
 
 
             <p><Link to='/'>HomePage</Link></p>
