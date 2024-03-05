c9e618d83a763e0a542cdf517d2369dc4c768366
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-03-05 22:16:17 +0300

    React-Tasks

--- react-tasks/src/page/Task84/Task84.js
+++ react-tasks/src/page/Task84/Task84.js
@@ -17,7 +17,7 @@ function Task84() {
         }
     }
     return (
-        <body style={{ backgroundColor: result.background, color: result.color, width: 1500, height: 900, margin: 0, padding: 0 }}>
+        <body style={{ backgroundColor: result.background, color: result.color, width: 1500, height: 900 }}>
             <div>
                 <p>Задача 84</p>
                 <p>Разработайте простое приложение для переключения между светлой и темной темами. Используйте useContext, чтобы
@@ -26,9 +26,9 @@ function Task84() {
                 <h1>Привет Ваня</h1>
                 <p>Как ты?</p>
                 <p>Ты стараешься в учёбе или занимаешься ерундой?</p>
-                <hr/>
+                <hr />
                 <h3>Нука соберись, осталось совсем немного, не ленись, последний, усиленный рывок!!!!!!!</h3>
-                <button onClick={clickButton}>Click me</button>
+                <button style={{ backgroundColor: result.color, color: result.background }} onClick={clickButton}>{result.flag ? 'black' : 'white'}</button>
                 <p><Link to='/'>HomePage</Link></p>
             </div>
         </body>
