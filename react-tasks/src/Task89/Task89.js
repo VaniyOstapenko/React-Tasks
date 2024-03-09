17a9dd95f4e2b3e7648aa6ae8a192e08e3ffefda
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-03-09 15:31:47 +0300

    React-Tasks

--- react-tasks/src/Task89/Task89.js
+++ react-tasks/src/Task89/Task89.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function reducer(state, action) {
     switch (action.type) {
-        case 'sum':
+        case 'val':
             return action.value;
         default:
             break;
@@ -22,8 +22,8 @@ function Task89() {
                 Посчитать сумму 2 input. По клику на кнопку отображать
                 результат в  консоль</p>
 
-            <input onChange={(e) => dispatch1({ type: 'sum', value: e.target.value })} type="text" />
-            <input onChange={(e) => dispatch2({ type: 'sum', value: e.target.value })} type="text" />
+            <input onChange={(e) => dispatch1({ type: 'val', value: e.target.value })} type="text" />
+            <input onChange={(e) => dispatch2({ type: 'val', value: e.target.value })} type="text" />
             <button onClick={() => console.log(Number(inpVal1) + Number(inpVal2))}>Click me</button >
             <p><Link to='/'>HomePage</Link></p>
         </>
